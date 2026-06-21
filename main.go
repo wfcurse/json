3dce56fcd5cf76d947c2b5d2ff275596ba567d55
wfcurse <241868221+wfcurse@users.noreply.github.com>
2026-06-21 18:36:24 +0700

    woek json

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 	"sync"
+	"time"
 )
 
 type Payment struct {
@@ -13,25 +13,51 @@ type Payment struct {
 	USD         int64  `json:"usd"`
 	FullName    string `json:"fullName"`
 	Address     string `jsin:"address"`
+	Time        time.Time
+}
+
+type HttpRespons struct {
+	Money          int64
+	PaymentHistory []Payment
 }
 
 var mtx = sync.Mutex{}
 var money int64 = 1000
 var paymentHistory = make([]Payment, 0)
 
-func payHandler(w http.ResponseWriter, r *http.Request) {
+func (p Payment) Println() {
+	fmt.Println("Descripton: ", p.Description)
+	fmt.Println("usd: ", p.USD)
+	fmt.Println("FullName: ", p.FullName)
+	fmt.Println("Address: ", p.Address)
+}
 
-	httpBody, err := io.ReadAll(r.Body)
-	if err != nil {
+func (p Payment) Validate() bool {
+
+	if p.USD == 0 {
+		return false
+	}
+
+	if p.Address == "" {
+		return false
+	}
+
+	return true
+}
+
+func payHandler(w http.ResponseWriter, r *http.Request) {
+	var payment Payment
+	if err := json.NewDecoder(r.Body).Decode(&payment); err != nil {
 		fmt.Println("ошибка при выполнении функции payHendler", err)
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
-	var payment Payment
-	json.Unmarshal(httpBody, &payment)
+	payment.Time = time.Now()
+
+	payment.Println()
 
 	mtx.Lock()
-	defer mtx.Unlock()
 
 	if money-payment.USD >= 0 {
 		money -= payment.USD
@@ -39,9 +65,25 @@ func payHandler(w http.ResponseWriter, r *http.Request) {
 
 	paymentHistory = append(paymentHistory, payment)
 
-	fmt.Println("money: ", money)
-	fmt.Println("paymentHistory", paymentHistory)
+	httpRespons := HttpRespons{
+		Money:          money,
+		PaymentHistory: paymentHistory,
+	}
 
+	b, err := json.MarshalIndent(httpRespons, "", "	")
+	if err != nil {
+		fmt.Println("err", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if _, err := w.Write(b); err != nil {
+		fmt.Println("err", err)
+
+		return
+	}
+
+	mtx.Unlock()
 }
 
 func main() {
