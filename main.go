a74e641ab7a90e875d867100a08a2e0ee67a9d1e
wfcurse <241868221+wfcurse@users.noreply.github.com>
2026-06-21 10:37:54 +0700

    update w json

--- main.go
+++ main.go
@@ -1,67 +1,40 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
-	"strconv"
-	"strings"
 	"sync"
 )
 
 type Payment struct {
-	Description string
-	USD         int64
+	Description string `json:"descriotion"`
+	USD         int64  `json:"usd"`
+	FullName    string `json:"fullName"`
+	Address     string `jsin:"address"`
 }
 
 var mtx = sync.Mutex{}
 var money int64 = 1000
 var paymentHistory = make([]Payment, 0)
 
-func readAmount(w http.ResponseWriter, r *http.Request) (int64, []string, error) {
-
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		fmt.Println("Ошибка чтения тела запроса")
-		return 0, nil, err
-	}
-
-	bodyString := strings.TrimSpace(string(body))
-
-	bodyStringParts := strings.SplitN(bodyString, ",", 2)
-
-	if len(bodyStringParts) != 2 {
-		http.Error(w, "Ошибка запроса", http.StatusBadRequest)
-		return 0, nil, err
-	}
-
-	usd, err := strconv.ParseInt(bodyStringParts[0], 10, 64)
-	if err != nil {
-		http.Error(w, "Ошибка перевода string в int", http.StatusBadRequest)
-		fmt.Println("Ошибка перевода string в int")
-		return 0, nil, nil
-	}
-
-	return usd, bodyStringParts, nil
-
-}
-
 func payHandler(w http.ResponseWriter, r *http.Request) {
-	usd, bodyStringParts, err := readAmount(w, r)
 
+	httpBody, err := io.ReadAll(r.Body)
 	if err != nil {
-		fmt.Println("ошибка при выполнении функции payHendler")
-	}
-	payment := Payment{
-		Description: bodyStringParts[1],
-		USD:         usd,
+		fmt.Println("ошибка при выполнении функции payHendler", err)
+		return
 	}
 
-	defer mtx.Unlock()
+	var payment Payment
+	json.Unmarshal(httpBody, &payment)
+
 	mtx.Lock()
+	defer mtx.Unlock()
 
-	if money-usd >= 0 {
-		money -= usd
+	if money-payment.USD >= 0 {
+		money -= payment.USD
 	}
 
 	paymentHistory = append(paymentHistory, payment)
